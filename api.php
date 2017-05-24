bdaf6a025ebaf8e0dd0c0b85d014413b0a788465
mipcc <mipcc.condo.2016@gmail.com>
2017-05-24 18:26:06 -0400

    'm'

--- api.php
+++ api.php
@@ -2554,7 +2554,7 @@ class PHP_CRUD_API {
 //config nube
 //'hostname'=>'localhost','username'=>'id911945_root','password'=>'12345','database'=>'id911945_mn','port'=>'3306', 	
 //config openshift
-'hostname'=>'localhost','username'=>'adminG5xMedX','password'=>'Ek8d8YWA3Iuq','database'=>'manantial','port'=>'3306', 	
+//'hostname'=>'localhost','username'=>'adminG5xMedX','password'=>'Ek8d8YWA3Iuq','database'=>'manantial','port'=>'3306', 	
 	'charset'=>'utf8'
  ));
  $api->executeCommand();
