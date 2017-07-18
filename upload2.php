c0bc42160e8c5220579f660a00a9f431da784aae
Chamorro Torres <selah.music.adorno@gmail.com>
2017-07-18 11:22:10 -0300

    'm'

--- upload2.php
+++ upload2.php
@@ -7,7 +7,7 @@ $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
 if(isset($_POST["submit"])) {
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
     if($check !== false) {
-        // echo "File is an image - " . $check["mime"] . ".";
+        echo "File is an image - " . $check["mime"] . ".";
         $uploadOk = 1;
     } else {
         echo "File is not an image.";
@@ -37,10 +37,9 @@ if ($uploadOk == 0) {
 // if everything is ok, try to upload file
 } else {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-		header('Location: tbl5b.html');
-       //echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+       echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
     } else {
-        //cho "Sorry, there was an error uploading your file.";
+        echo "Sorry, there was an error uploading your file.";
     }
 }
 ?>
